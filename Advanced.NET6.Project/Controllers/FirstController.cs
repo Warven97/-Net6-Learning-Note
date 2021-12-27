154fca296aa47c695134aae287f0d17bf2b97c4c
Warven97 <122793663@qq.com>
2021-12-27 09:39:34 +0800

    create a asp.net project as learning space

---
+++ Advanced.NET6.Project/Controllers/FirstController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Advanced.NET6.Project.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index()
+        {
+            {
+
+            }
+            ViewBag.User1 = "张三";
+            ViewData["User2"] = "李四";
+
+            return View();
+        }
+    }
+}
---
+++ Advanced.NET6.Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Advanced.NET6.Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
